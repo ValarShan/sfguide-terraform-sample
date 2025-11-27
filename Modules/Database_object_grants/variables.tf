b33cde417b6133ef4a07fdda1ef18e50073862a5
Valar Shan <valar.shan@Rathbones.com>
2025-11-27 17:56:53 +0000

    Commit_changes

--- Modules/Database_object_grants/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "env" {}
-variable "env_raw_db" {}
-variable "env_raw_db_reader_role" {}
-variable "env_raw_db_writer_role" {}
--- Modules/Role_grants/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "env" {}
-variable "env_raw_db_reader_role" {}
-variable "env_raw_db_writer_role" {}
-variable "env_analyst_role"{}
-variable "env_loader_role" {}
