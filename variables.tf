6504ed590e4184bc7b22905ca8373ee5ed56b20c
Valar Shan <valar.shan@Rathbones.com>
2025-11-28 14:40:51 +0000

    user changed

--- variables.tf
+++ variables.tf
@@ -4,8 +4,8 @@ variable "warehouse_size" { default = "XSMALL" }
 
 variable "retention_days" { default = 1 }
 
-variable "organization_name" {default ="PQFCSPQ-IF19845"}
+variable "organization_name" {default ="PQFCSPQ"}
 
 variable "private_key_path" {default ="~/.ssh/snowflake_tf_snow_key.p8"}
 
-variable "user" {default ="VALARSHAN"}
+variable "user" {default ="PQFCSPQ-IF19845"}
