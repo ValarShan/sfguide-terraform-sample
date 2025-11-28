dbe6b218579c7dd18d486bad36d98af357db5c60
Valar Shan <valar.shan@Rathbones.com>
2025-11-28 14:11:18 +0000

    role.variable.tf amended

--- modules/Role_grants/variables.tf
+++ modules/Role_grants/variables.tf
@@ -1,5 +1,5 @@
 variable "env" {}
 variable "env_raw_db_reader_role" {}
 variable "env_raw_db_writer_role" {}
-variable "env_analyst_role"{}
-variable "env_loader_role" {}
+variable "env_raw_db_analyst_role"{}
+variable "env_raw_db_loader_role" {}
