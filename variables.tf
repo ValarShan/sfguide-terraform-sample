44f474054f05b82336b8cab891afbac68ee59314
Valar Shan <valar.shan@Rathbones.com>
2025-11-28 14:29:01 +0000

    org_name_changedwitheuWest

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "warehouse_size" { default = "XSMALL" }
 
 variable "retention_days" { default = 1 }
 
-variable "organization_name" {default ="pqfcspq-if19845"}
+variable "organization_name" {default ="if19845.eu-west-1"}
 
 variable "private_key_path" {default ="~/.ssh/snowflake_tf_snow_key.p8"}
 
