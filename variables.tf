c860dfd306db6a8e38b415aadc3ea58d5a5e0559
Valar Shan <valar.shan@Rathbones.com>
2025-12-01 15:00:11 +0000

    variableadded

--- variables.tf
+++ variables.tf
@@ -1,13 +1,7 @@
-variable "env" { default = "TRAIL" }
-
-variable "warehouse_size" { default = "XSMALL" }
-
-variable "retention_days" { default = 1 }
-
+variable "user"     { default = "VALARSHAN" }
 variable "password" { default = "Christmas@2025" }
-
-variable "user" { default = "VALARSHAN" }
-
-variable "account" { default = "PQFCSPQ-IF19845" }
-
-variable "role" { default = "ACCOUNTADMIN" }
+variable "account"  { default = "PQFCSPQ-IF19845" }
+variable "role"     { default = "ACCOUNTADMIN" }
+variable "env"      { default = "TRAIL" }
+variable "warehouse_size" { default = "XSMALL" }
+variable "retention_days" { default = 1 }
