2014cbc39b8992e9c73716ea2134256a8c7573b5
Valar Shan <valar.shan@Rathbones.com>
2025-12-01 13:54:37 +0000

    fix(provider): switch username to user argument

--- variables.tf
+++ variables.tf
@@ -4,12 +4,10 @@ variable "warehouse_size" { default = "XSMALL" }
 
 variable "retention_days" { default = 1 }
 
-#variable "organization_name" {default ="PQFCSPQ.eu-west-1"}
+variable "password" { default = "Christmas@2025" }
 
-variable "password" {default ="Christmas@2025"}
+variable "user" { default = "VALARSHAN" }
 
-variable "user" {default ="VALARSHAN"}
+variable "account" { default = "PQFCSPQ-IF19845" }
 
-variable "account" {default ="PQFCSPQ-IF19845"}
-
-variable "role" {default = "ACCOUNTADMIN"}
+variable "role" { default = "ACCOUNTADMIN" }
