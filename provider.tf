c94f2b40129aab666a828d4aa004750673018c8b
Valar Shan <valar.shan@Rathbones.com>
2025-12-01 14:04:13 +0000

    hardcodedproviderblock

--- provider.tf
+++ provider.tf
@@ -6,10 +6,17 @@ terraform {
     }
   }
 }
-
+/*
 provider "snowflake" {
   user             = var.user
   password         = var.password   
   account          = var.account
   role             = var.role
+}
+*/
+provider "snowflake" {
+  user     = "VALARSHAN"
+  password = "Christmas@2025"
+  account  = "PQFCSPQ-IF19845"
+  role     = "ACCOUNTADMIN"
 }
