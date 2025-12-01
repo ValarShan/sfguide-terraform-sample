c80fe83ef84e021660018330bd84e30e95207213
Valar Shan <valar.shan@Rathbones.com>
2025-12-01 15:55:33 +0000

    orgnameadded

--- provider.tf
+++ provider.tf
@@ -7,12 +7,6 @@ terraform {
   }
 }
 
-provider "snowflake" {
-  user             = var.user
-  password         = var.password   
-  account          = var.account
-  role             = var.role
-}
 /*
 provider "snowflake" {
   user     = "VALARSHAN"
@@ -21,3 +15,22 @@ provider "snowflake" {
   role     = "ACCOUNTADMIN"
 }
 */
+
+locals {
+  organization_name   = "PQFCSPQ"
+  account_name        = "IF19845"
+  user                = "VALARSHAN" 
+  password            = "Christmas@2025"   
+  account             = "PQFCSPQ-IF19845"
+  role                = "ACCOUNTADMIN"
+
+}
+
+provider "snowflake" {
+  organization_name = local.organization_name
+  account_name      = local.account_name
+  user             = local.user
+  password         = local.password   
+  account          = local.account
+  role             = local.role
+}
--- variables.tf
+++ variables.tf
@@ -1,7 +1,3 @@
-variable "user"     { default = "VALARSHAN" }
-variable "password" { default = "Christmas@2025" }
-variable "account"  { default = "PQFCSPQ-IF19845" }
-variable "role"     { default = "ACCOUNTADMIN" }
 variable "env"      { default = "TRAIL" }
 variable "warehouse_size" { default = "XSMALL" }
 variable "retention_days" { default = 1 }
