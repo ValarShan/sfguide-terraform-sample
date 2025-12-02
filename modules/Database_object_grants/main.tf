8ac325a79a519239b22c3dd32c272c8ccfdac383
Valar Shan <valar.shan@Rathbones.com>
2025-12-02 15:50:54 +0000

    Version0.93_Database_grants_maintf_resourceblock_updated

--- modules/Database_object_grants/main.tf
+++ modules/Database_object_grants/main.tf
@@ -10,6 +10,8 @@ terraform {
 #DATABASE LEVEL GRANTS
 ## RAW_DB GRANTS
 ## "env_raw_db_reader_grant" - future we need to parametrised the envs too
+
+/*commented out below resources for 0.93 upgrade
 resource "snowflake_database_grant" "env_raw_db_reader_grant" {
   database_name          = var.env_raw_db
   privilege              = "USAGE"
@@ -24,3 +26,32 @@ resource "snowflake_database_grant" "env_raw_db_writer_grant" {
   enable_multiple_grants = true
   roles                  = [var.env_raw_db_writer_role]
 }
+*/
+# Old resource removed
+removed {  
+  from = snowflake_database_grant.env_raw_db_reader_grant  
+  lifecycle {  
+    destroy = false  
+  }  
+}
+# New resource definition
+resource "snowflake_grant_privileges_to_account_role" "env_raw_db_reader_grant" {  
+  account_role_name    = "\"${var.env_raw_db_reader_role}\""  
+  privileges           = ["USAGE"]  
+  with_grant_option    = false  
+  on_account_object {  
+    object_name = "\"${var.env_raw_db}\""  
+    object_type = "DATABASE"  
+  }  
+}
+
+resource "snowflake_grant_privileges_to_account_role" "env_raw_db_writer_grant" {  
+  account_role_name    = "\"${var.env_raw_db_writer_role}\""  
+  privileges           = ["USAGE"]  
+  with_grant_option    = false  
+  on_account_object {  
+    object_name = "\"${var.env_raw_db}\""  
+    object_type = "DATABASE"  
+  }  
+}
+  
