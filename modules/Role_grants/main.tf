b0526c566d1eb059412c167abe2d7a5ec73e031f
Valar Shan <valar.shan@Rathbones.com>
2025-12-02 16:08:12 +0000

    Version0.93_Rolegrants_resourceBlock_updated

--- modules/Role_grants/main.tf
+++ modules/Role_grants/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 }
-
+/*
 # Base Role Grants
 resource "snowflake_role_grants" "env_raw_db_reader_role_grant" {
   role_name = var.env_raw_db_reader_role
@@ -17,4 +17,22 @@ resource "snowflake_role_grants" "env_raw_db_writer_role_grant" {
   role_name = var.env_raw_db_writer_role
   roles     = [var.env_raw_db_loader_role]
 }
+*/
+# Old resource removed
+removed {  
+  from = snowflake_role_grants.env_raw_db_reader_role_grant  
+  lifecycle {  
+    destroy = false  
+  }  
+}
+# New resource definition
+# Grant a role to another role
+resource "snowflake_roles_grants_to_role" "env_raw_db_reader_role_grant" {
+  role_name = "\"${var.env_raw_db_reader_role}\""   # target role quoted
+  roles     = ["\"${var.var.env_raw_db_analyst_role}\""] # granted role quoted
+}
 
+resource "snowflake_roles_grants_to_role" "env_raw_db_writer_role_grant" {
+  role_name = "\"${var.env_raw_db_writer_role}\""   # target role quoted
+  roles     = ["\"${var.var.env_raw_db_loader_role}\""] # granted role quoted
+}
