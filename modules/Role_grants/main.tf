faaf829e2caa97e1e1b9b105e5e477a441a7623c
Valar Shan <valar.shan@Rathbones.com>
2025-12-03 11:32:29 +0000

    Maintf_Version_Updated_to_0.93

--- modules/Role_grants/main.tf
+++ modules/Role_grants/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "0.74.0"
+      version = "0.93.0"
     }
   }
 }
@@ -20,19 +20,47 @@ resource "snowflake_role_grants" "env_raw_db_writer_role_grant" {
 */
 # Old resource removed
 removed {  
-  from = snowflake_role_grants.env_raw_db_reader_role_grant  
+  from = snowflake_role_grants.env_raw_db_reader_role_grant 
+  lifecycle {  
+    destroy = false  
+  }  
+}
+
+removed {  
+  from = snowflake_role_grants.env_raw_db_writer_role_grant 
   lifecycle {  
     destroy = false  
   }  
 }
 # New resource definition
 # Grant a role to another role
-resource "snowflake_roles_grants_to_role" "env_raw_db_reader_role_grant" {
-  role_name = "\"${var.env_raw_db_reader_role}\""   # target role quoted
-  roles     = ["\"${var.var.env_raw_db_analyst_role}\""] # granted role quoted
+
+resource "snowflake_grant_privileges_to_account_role" "env_raw_db_reader_grant" {
+  for_each    = tomap({
+      env_raw_db_analyst_role-fiscal       = tomap({role = var.env_raw_db_analyst_role, schema = "fiscal"})
+      env_raw_db_reader_role-fiscal        = tomap({role = var.env_raw_db_reader_role, schema = "fiscal"})
+  })
+  privileges = ["SELECT"]
+  account_role_name  = "\"${each.value.role}\""
+  on_schema_object {
+    all {
+      object_type_plural = "TABLES"
+      in_schema          = format("\"%s\".\"%s\"", var.env_raw_db, each.value.schema)
+    }
+  }
 }
 
-resource "snowflake_roles_grants_to_role" "env_raw_db_writer_role_grant" {
-  role_name = "\"${var.env_raw_db_writer_role}\""   # target role quoted
-  roles     = ["\"${var.var.env_raw_db_loader_role}\""] # granted role quoted
+resource "snowflake_grant_privileges_to_account_role" "env_raw_db_writer_grant" {
+  for_each    = tomap({
+      env_raw_db_loader_role-fiscal       = tomap({role = var.env_raw_db_loader_role, schema = "fiscal"})
+      env_raw_db_writer_role-fiscal        = tomap({role = var.env_raw_db_writer_role, schema = "fiscal"})
+  })
+  privileges = ["SELECT"]
+  account_role_name  = "\"${each.value.role}\""
+  on_schema_object {
+    all {
+      object_type_plural = "TABLES"
+      in_schema          = format("\"%s\".\"%s\"", var.env_raw_db, each.value.schema)
+    }
+  }
 }
