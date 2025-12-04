9ebc2d9257c265b024021c9e132a45ed68f547b0
Valar Shan <valar.shan@Rathbones.com>
2025-12-04 14:52:55 +0000

    AuthenticatingviaPrivatekey

--- provider.tf
+++ provider.tf
@@ -6,10 +6,32 @@ terraform {
     }
   }
 }
-
+/*
 provider "snowflake" {
   user             = var.login_name
   password         = var.password   
   account          = var.account_name
   role             = "SYSADMIN"
+}
+
+terraform {
+  required_providers {
+    snowflake = {
+      source = "snowflakedb/snowflake"
+    }
+  }
+}
+*/
+
+locals {
+  account_name      = "IF19845"
+  private_key_path  = "~/.ssh/snowflake_tf_snow_key.p8"
+}
+
+provider "snowflake" {
+  account_name      = local.account_name
+  user              = "TERRAFORM_SVC"
+  role              = "SYSADMIN"
+  authenticator     = "JWT"   # updated for v0.74
+  private_key       = file(local.private_key_path)
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "env"      { default = "TRAIL" }
 variable "warehouse_size" { default = "XSMALL" }
 variable "retention_days" { default = 1 }
+/*
 variable "login_name"     { default = "VALARSHAN" }
 variable "password" { default = "Christmas@2025" }
 variable "account_name"  { default = "PQFCSPQ-IF19845" }
+*/
 
